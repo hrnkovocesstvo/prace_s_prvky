ca4c535adc57eeab202599bdcb3ea00dbfbf50bd
hrnkovocesstvopcdole <BabnicA.itb25@vsps-su.cz>
2026-02-18 10:40:44 +0100

    funkcni prvni cast

--- prace_s_prvky/Form1.Designer.cs
+++ prace_s_prvky/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericPocet = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDoit = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPocet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericPocet
+            // 
+            this.numericPocet.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(238)));
+            this.numericPocet.Location = new System.Drawing.Point(583, 127);
+            this.numericPocet.Name = "numericPocet";
+            this.numericPocet.Size = new System.Drawing.Size(120, 30);
+            this.numericPocet.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(515, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Počet prvků v poli";
+            // 
+            // buttonDoit
+            // 
+            this.buttonDoit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDoit.Location = new System.Drawing.Point(546, 246);
+            this.buttonDoit.Name = "buttonDoit";
+            this.buttonDoit.Size = new System.Drawing.Size(197, 89);
+            this.buttonDoit.TabIndex = 2;
+            this.buttonDoit.Text = "doit";
+            this.buttonDoit.UseVisualStyleBackColor = true;
+            this.buttonDoit.Click += new System.EventHandler(this.buttonDoit_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox.Location = new System.Drawing.Point(46, 70);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(452, 265);
+            this.textBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonDoit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericPocet);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericPocet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericPocet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDoit;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- prace_s_prvky/Form1.cs
+++ prace_s_prvky/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace prace_s_prvky
 {
@@ -15,6 +16,35 @@ namespace prace_s_prvky
         public Form1()
         {
             InitializeComponent();
+        }
+
+        List<int> ciselnarada = new List<int>();
+
+        public void zobrazit(String text)
+        {
+            textBox.Text = $"Pole: {text} + {Environment.NewLine}" + text;
+                            
+        }
+
+        private void buttonDoit_Click(object sender, EventArgs e)
+        {
+            if (numericPocet.Value == 0) { return; }
+
+
+            String toot = string.Empty; 
+            Random rn = new Random();
+            for (int i = 0; i < numericPocet.Value; i++)
+            {
+                ciselnarada.Add(rn.Next(-100, 100));
+            }
+            
+            for (int i = 0; i < ciselnarada.Count; i++)
+            {
+                toot += $"{ciselnarada[i]}, "; 
+
+            }
+            toot = toot.Substring(0, toot.Length - 2);
+            zobrazit(toot);
         }
     }
 }
